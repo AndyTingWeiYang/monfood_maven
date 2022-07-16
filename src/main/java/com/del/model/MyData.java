270940168d454dcf5263b64bf33064c3074fb453
wii <wii6436@gmail.com>
2022-07-16 17:25:56 +0800

    add MyData, DelVO, DelDAO, DelDAOImpl, TestDelDAO move to new repo

---
+++ src/main/java/com/del/model/MyData.java
@@ -0,0 +1,12 @@
+package com.del.model;
+
+public class MyData {
+	public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+
+	public static final String URL = "jdbc:mysql://localhost:3306/java?serverTimezone=Asia/Taipei";
+
+	public static final String USER = "root";
+
+	public static final String PASSWORD = "password";
+
+}
