9f934fa59f58a6495722953e3946250b95b2d3ac
wii <wii6436@gmail.com>
2022-07-18 11:41:44 +0800

    add model of adminLocation as title

---
+++ src/main/java/com/model/adminLocation/TestAdminLocation.java
@@ -0,0 +1,50 @@
+package com.model.adminLocation;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class TestAdminLocation {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+//insert
+//		System.out.println("enter zipcode plz");
+//		Integer zipCode = sc.nextInt();
+//		System.out.println("enter adminRegion plz");
+//		String adminRegion = sc.next();
+//		
+//		sc.close();
+//		
+//		AdminLocationVO adminLocationVO = new AdminLocationVO(zipCode, adminRegion);
+//		AdminLocationDAO daoInsert = new AdminLocationDAOImpl();
+//		daoInsert.add(adminLocationVO);
+		
+//update by zipCode
+//		System.out.println("enter zipcode plz");
+//		Integer zipCode = sc.nextInt();
+//		System.out.println("enter adminRegion plz");
+//		String adminRegion = sc.next();
+//		
+//		sc.close();
+//		
+//		AdminLocationVO adminLocationVO = new AdminLocationVO(zipCode, adminRegion);
+//		AdminLocationDAO daoUpdate = new AdminLocationDAOImpl();
+//		daoUpdate.update(adminLocationVO);
+		
+//search by zipCode
+//		System.out.println("請輸入欲查詢之zipCode");
+//		Integer searchZipCode = sc.nextInt();
+//		AdminLocationDAO daoGet1 = new AdminLocationDAOImpl();
+//		AdminLocationVO bean = daoGet1.findByzipCode(searchZipCode);
+//		System.out.println(bean.toString());
+	
+//getAll
+//		AdminLocationDAO daoGetall = new AdminLocationDAOImpl();
+//		List<AdminLocationVO> beans = daoGetall.getAll();
+//		for(AdminLocationVO adminLocationVO : beans)
+//			System.out.println(adminLocationVO.toString());
+//	
+	
+	}
+
+}
