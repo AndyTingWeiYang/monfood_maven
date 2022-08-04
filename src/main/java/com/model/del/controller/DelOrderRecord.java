0f4b1ea3213df9fc8858eac673f57791f8e15ac2
wii <wii6436@gmail.com>
2022-08-04 09:41:32 +0800

    update DelDAOImpl, DelOrderRecord, del_order_record.html for fixing the comment getted problem

--- src/main/java/com/model/del/controller/DelOrderRecord.java
+++ src/main/java/com/model/del/controller/DelOrderRecord.java
@@ -5,6 +5,7 @@ import java.io.PrintWriter;
 import java.sql.Timestamp;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Base64;
 import java.util.List;
 
@@ -53,11 +54,15 @@ public class DelOrderRecord extends HttpServlet {
 		System.out.println("afterG");
 		JsonElement sdElement = json.get("startDate");
 		String sdString = String.valueOf(sdElement);
-		Timestamp startDate = Timestamp.valueOf(sdString);          
+		String sdString2 = sdString.replace("\"", "") + " 00:00:00";
+		System.out.println(sdString2);
+		Timestamp startDate = Timestamp.valueOf(sdString2);          
 		
 		JsonElement edElement = json.get("endDate");
 		String edString = String.valueOf(edElement);
-		Timestamp endDate = Timestamp.valueOf(edString);
+		String edString2 = edString.replace("\"", "") + " 00:00:00";
+		System.out.println(edString2);
+		Timestamp endDate = Timestamp.valueOf(edString2);
 		
 		System.out.println(startDate);
 		System.out.println(endDate);
@@ -69,8 +74,9 @@ public class DelOrderRecord extends HttpServlet {
 		
 		
 		com.model.del.service.DelService service = new DelServiceImpl();
-		List<String> result = service.getDelRecord(startDate, endDate, loginDelVO.getDelID());
-		
+		List result = new ArrayList();
+		result = service.getDelRecord(startDate, endDate, loginDelVO.getDelID());
+		respObj.add("result", gson.toJsonTree(result));
 		
 		
 //		JsonObject respObj = new JsonObject();
@@ -80,7 +86,7 @@ public class DelOrderRecord extends HttpServlet {
 		System.out.println(result);
 ////		DelService service = new DelService();
 ////		DelVO result = service.updateDel(delVO);
-		resp.getWriter().append(gson.toJson(result));
+		resp.getWriter().append(gson.toJson(respObj));
 		
 		
 		
