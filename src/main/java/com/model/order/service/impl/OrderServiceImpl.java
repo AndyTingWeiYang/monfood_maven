e02debcb1731a925bc87bb6e11cc7a00fb22433b
AndyTingWeiYang <103553470+AndyTingWeiYang@users.noreply.github.com>
2022-08-05 00:22:59 +0800

    update ECPAY for orders update userOrder to add rating function into the page

--- src/main/java/com/model/order/service/impl/OrderServiceImpl.java
+++ src/main/java/com/model/order/service/impl/OrderServiceImpl.java
@@ -1,8 +1,20 @@
 package com.model.order.service.impl;
 
+import java.nio.channels.AcceptPendingException;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Optional;
 
+import org.hibernate.loader.entity.NaturalIdEntityJoinWalker;
+import org.quartz.jobs.ee.jms.SendDestinationMessageJob;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.model.monster.IMonsterDAO;
 import com.model.monster.MonsterJDBCDAO;
 import com.model.monster.MonsterVO;
@@ -10,11 +22,19 @@ import com.model.order.OrderVO;
 import com.model.order.dao.OrderDAO;
 import com.model.order.dao.impl.OrderJDBCDAOimpl;
 import com.model.order.service.OrderService;
+import com.model.orderdetail.OrderDetailVO;
+import com.model.orderdetail.dao.OrderDetailDAO;
+import com.model.orderdetail.dao.impl.OrderDetailDAOimpl;
+import com.model.orderdetail.service.OrderDetailService;
+import com.model.orderdetail.service.impl.OrderDetailServiceimpl;
 import com.model.promotelist.PromoteListVO;
 import com.model.promotelist.dao.PromoteListDAO;
 import com.model.promotelist.dao.Impl.PromoteListJDBCDAO;
 import com.model.user.dao.UserDAO;
 import com.model.user.dao.impl.UserDAOImpl;
+
+import ecpay.payment.integration.AllInOne;
+import ecpay.payment.integration.domain.AioCheckOutALL;
 
 public class OrderServiceImpl implements OrderService {
 	
@@ -63,21 +83,66 @@ public class OrderServiceImpl implements OrderService {
 	}
 	
 	@Override
-	public Integer createOrder(OrderVO orderVO) {
-		Integer generatedKey = null;
+	public String ecpayValidation(List<String> nameList, Integer orderId, OrderVO orderVO) {
 		
+		Optional<String> reduce = nameList.stream().reduce((String acc, String curr) ->{
+			return acc + "#" + curr;
+		});
+		String itemName = reduce.get();
+			
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		String orderDate = sdf.format(new Date());
+		AllInOne allInOne = new AllInOne("");
+		AioCheckOutALL aioCheckOutALL = new AioCheckOutALL();
+		aioCheckOutALL.setMerchantTradeNo(orderId + "MonFood");
+		aioCheckOutALL.setMerchantTradeDate(orderDate);
+		aioCheckOutALL.setTotalAmount(orderVO.getTotal().toString());
+		aioCheckOutALL.setTradeDesc("test");
+		aioCheckOutALL.setItemName(itemName);
+		aioCheckOutALL.setClientBackURL("http://localhost:8080/monfood_maven/searching.html");
+		aioCheckOutALL.setReturnURL("http://localhost:8080/monfood_maven/searching.html");
+		aioCheckOutALL.setNeedExtraPaidInfo("N");
+		
+		return allInOne.aioCheckOut(aioCheckOutALL, null);
+		
+	}
+	
+	@Override
+	public Integer createOrder(List<com.google.gson.JsonElement> list , OrderVO orderVO) {
+		Gson gson = new Gson();
+		Integer generatedKey = null;
+		OrderDetailService orderDetailService = new OrderDetailServiceimpl();
+		
+		// no promote code
 		if (orderVO.getPromoteId() == null) {
 			generatedKey = dao.insertNoPromote(orderVO);
 			if(generatedKey == null) {
-				return null;
+				return -1;
 			}
-			return generatedKey;
-		}
+			
+			for (JsonElement jsonElement : list) {
+				OrderDetailVO orderDetailVO = new OrderDetailVO();
+				orderDetailVO = gson.fromJson(jsonElement, OrderDetailVO.class);
+				orderDetailVO.setOrderId(generatedKey);
+				orderDetailService.createOrderDetail(orderDetailVO);
+			}
 		
+			return generatedKey;
+			
+		} 	
+		
+		// with promote code
 		generatedKey = dao.insert(orderVO);
 		if(generatedKey == null) {
-			return null;
+			return -1;
 		}
+		for (JsonElement jsonElement : list) {
+			OrderDetailVO orderDetailVO = new OrderDetailVO();
+			orderDetailVO = gson.fromJson(jsonElement, OrderDetailVO.class);
+			orderDetailVO.setOrderId(generatedKey);
+			orderDetailService.createOrderDetail(orderDetailVO);
+		}
+		
 		return generatedKey;
 	}
 	
@@ -146,6 +211,17 @@ public class OrderServiceImpl implements OrderService {
 			orderTimes = 0;
 		}
 		return orderTimes;
+	}
+	
+	@Override
+	public Integer updateRating(OrderVO orderVO) {
+		
+		if (orderVO == null) {
+			return -1;
+		}
+		
+		dao.update(orderVO);
+		return 1;
 	}
 	
 	
