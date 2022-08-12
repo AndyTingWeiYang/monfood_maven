e29da512aa4db7aabd9816eaf82b3183a7ae958f
Et_Lin <lioveamy0820@gmail.com>
2022-08-12 10:34:31 +0800

    update ProductPicServlet.java , ResInfoPreviewServlet.java , ResDAOImpl.java add food.png

--- src/main/java/com/model/res/controller/ResInfoPreviewServlet.java
+++ src/main/java/com/model/res/controller/ResInfoPreviewServlet.java
@@ -36,7 +36,7 @@ public class ResInfoPreviewServlet extends HttpServlet {
 			throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html;charset=UTF-8");
-		HttpSession session = request.getSession();
+		HttpSession session = request.getSession(false);
 		Integer resID = (Integer) session.getAttribute("resID");
 		Map<String, Object> resMap = resService.selectResInfo(resID);
 		
