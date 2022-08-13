462c79f22c3d9e531c31b639beec371df15a8bae
emilywu510 <emily129963@gmail.com>
2022-08-13 23:21:28 +0800

    update buttonContent.js for onload event

--- src/main/webapp/js/buttonContent.js
+++ src/main/webapp/js/buttonContent.js
@@ -121,163 +121,175 @@ function refreshChat() {
 
 
 //配對者資訊ajax
-$.ajax({
-  url: "PairInfoServlet",
-  type: "GET",
-  dataType: "json",
-  success: function (data) {
-    if (data.length == 0){
-      console.log("a");
-      $("#acceptPairBtn").hide();
-      $("#refusePairBtn").hide();
-      let pairHtml = `<div>很抱歉，您今日無任何配對</div>`;
-      $("#intro").append(pairHtml);
-      $("#intro").css("background-color", "white");
+function pair(){
+  $.ajax({
+    url: "PairInfoServlet",
+    type: "GET",
+    dataType: "json",
+    success: function (data) {
+      if (data.length == 0){
+        console.log("a");
+        $("#acceptPairBtn").hide();
+        $("#refusePairBtn").hide();
+        let pairHtml = `<div>很抱歉，您今日無任何配對</div>`;
+        $("#intro").append(pairHtml);
+        $("#intro").css("background-color", "white");
+  
+      }
+      var base64String = btoa(
+        String.fromCharCode.apply(null, new Uint8Array(data[0].profilePic))
+      );
+      
+      $("#headshot").attr("src", `data:image/png;base64,${base64String}`);
+      $(".name").append(data[0].userName);
+      $("#selfIntro").append(data[0].userProfile);
+      $("#name1").append(data[0].userName);
+    },
+  });
+  console.log("s");
+}
 
-    }
-    var base64String = btoa(
-      String.fromCharCode.apply(null, new Uint8Array(data[0].profilePic))
-    );
-    
-    $("#headshot").attr("src", `data:image/png;base64,${base64String}`);
-    $(".name").append(data[0].userName);
-    $("#selfIntro").append(data[0].userProfile);
-    $("#name1").append(data[0].userName);
-  },
-});
 
 //更新答案ajax
 // 到其他人頁面改為動態
-$(document).on("click","#acceptPairBtn, #refusePairBtn", function (e) {
-  console.log("aa");
-  fetch("UpdateAnswerServlet", {
-    method: "POST",
-    headers: {
-      "content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      useraAnswer: e.target.value,
-      userbAnswer: e.target.value,
-    }),
+function update() {
+  $(document).on("click","#acceptPairBtn, #refusePairBtn", function (e) {
+    console.log("aa");
+    fetch("UpdateAnswerServlet", {
+      method: "POST",
+      headers: {
+        "content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        useraAnswer: e.target.value,
+        userbAnswer: e.target.value,
+      }),
+    });
   });
-});
+  
+  $("#acceptPairBtn, #refusePairBtn").on("click", function (e) {
+    console.log("aa");
+    fetch("UpdateAnswerServlet", {
+      method: "POST",
+      headers: {
+        "content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        useraAnswer: e.target.value,
+        userbAnswer: e.target.value,
+      }),
+    });
+  });
+  console.log("uu");
+}
 
-$("#acceptPairBtn, #refusePairBtn").on("click", function (e) {
-  console.log("aa");
-  fetch("UpdateAnswerServlet", {
-    method: "POST",
-    headers: {
-      "content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      useraAnswer: e.target.value,
-      userbAnswer: e.target.value,
-    }),
-  });
-});
 
 
 //訊息列表+聊天室ajax
-$.ajax({
-  url: "PairListServlet",
-  type: "GET",
-  dataType: "json",
-  success: function (data) {
-    if (data.length == 0) {
-      let noFriends = `<div>您目前無任何好友，快看看今日配對 ! </div>`;
-      $(".list-group").append(noFriends);
-    }
-    console.log(data);
-    for (var i = 0; i < data.length; i++) {
-      var base64String = btoa(
-        String.fromCharCode.apply(null, new Uint8Array(data[i].profilePic))
-      );
-      var html = "";
-      html += `              <a
-      href="#"
-      class="list-group-item list-group-item-action findChatId"
-      aria-current="true"
-      data-bs-toggle="modal"
-      data-bs-target="#chatRoomModal${data[i].userId}"
-      data-value = "${data[i].userId}"
-
+function chat() {
+  $.ajax({
+    url: "PairListServlet",
+    type: "GET",
+    dataType: "json",
+    success: function (data) {
+      if (data.length == 0) {
+        let noFriends = `<div>您目前無任何好友，快看看今日配對 ! </div>`;
+        $(".list-group").append(noFriends);
+      }
+      console.log(data);
+      for (var i = 0; i < data.length; i++) {
+        var base64String = btoa(
+          String.fromCharCode.apply(null, new Uint8Array(data[i].profilePic))
+        );
+        var html = "";
+        html += `              <a
+        href="#"
+        class="list-group-item list-group-item-action findChatId"
+        aria-current="true"
+        data-bs-toggle="modal"
+        data-bs-target="#chatRoomModal${data[i].userId}"
+        data-value = "${data[i].userId}"
+  
+      >
+        <div class="d-flex w-100">
+          <div class="avatarList"></div>
+          <div id="chatList">
+            <h5 class="mb-1">${data[i].userName}</h5>
+            <p id="chatContent" class="mb-1">開始聊天吧!</p>
+          </div>
+          
+        </div>
+      </a>
+      `;
+  
+        var html2 = "";
+        html2 += `<div
+      class="modal fade"
+      id="chatRoomModal${data[i].userId}"
+      tabindex="-1"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
     >
-      <div class="d-flex w-100">
-        <div class="avatarList"></div>
-        <div id="chatList">
-          <h5 class="mb-1">${data[i].userName}</h5>
-          <p id="chatContent" class="mb-1">開始聊天吧!</p>
-        </div>
-        
-      </div>
-    </a>
-    `;
-
-      var html2 = "";
-      html2 += `<div
-    class="modal fade"
-    id="chatRoomModal${data[i].userId}"
-    tabindex="-1"
-    aria-labelledby="exampleModalLabel"
-    aria-hidden="true"
-  >
-    <div class="modal-dialog">
-      <div id="modalContentPair" class="modal-content">
-        <div id="modalHeaderPair" class="modal-header">
-          <h5>
-            <span class="logo">Mon</span><span class="logo2">Food</span>
-          </h5>
-          <button
-            type="button"
-            class="btn-close"
-            data-bs-dismiss="modal"
-            aria-label="Close"
-          ></button>
-        </div>
-        <div class="modal-body">
-          <div class="top_menu">
-            <div class="buttons">
-              <button
-                id="back"
-                data-bs-toggle="modal"
-                data-bs-target="#chatListModal"
-              >
-                <
-              </button>
-            </div>
-            <div class="title">${data[i].userName}</div>
+      <div class="modal-dialog">
+        <div id="modalContentPair" class="modal-content">
+          <div id="modalHeaderPair" class="modal-header">
+            <h5>
+              <span class="logo">Mon</span><span class="logo2">Food</span>
+            </h5>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
           </div>
-          <ul id= "messagesArea${data[i].userId}" class="messages message-area"></ul>
-
-          <div id="sendMsg" class="bottom_wrapper clearfix">
-            <div class="message_input_wrapper">
-              <input id = "msgInput${data[i].userId}" class="message_input" type="text" placeholder="請輸入文字. . . " onkeydown="if (event.keyCode == 13) sendMessage()"/>
+          <div class="modal-body">
+            <div class="top_menu">
+              <div class="buttons">
+                <button
+                  id="back"
+                  data-bs-toggle="modal"
+                  data-bs-target="#chatListModal"
+                >
+                  <
+                </button>
+              </div>
+              <div class="title">${data[i].userName}</div>
             </div>
-            <div class="send_message" onclick="sendMessage();">
-              <div class="icon"></div>
-              <div class="text">送出</div>
+            <ul id= "messagesArea${data[i].userId}" class="messages message-area"></ul>
+  
+            <div id="sendMsg" class="bottom_wrapper clearfix">
+              <div class="message_input_wrapper">
+                <input id = "msgInput${data[i].userId}" class="message_input" type="text" placeholder="請輸入文字. . . " onkeydown="if (event.keyCode == 13) sendMessage()"/>
+              </div>
+              <div class="send_message" onclick="sendMessage();">
+                <div class="icon"></div>
+                <div class="text">送出</div>
+              </div>
             </div>
           </div>
-        </div>
-
-        <div class="message_template">
-          <li class="message">
-            <div class="avatar"></div>
-            <div class="text_wrapper">
-              <div class="text"></div>
-            </div>
-          </li>
+  
+          <div class="message_template">
+            <li class="message">
+              <div class="avatar"></div>
+              <div class="text_wrapper">
+                <div class="text"></div>
+              </div>
+            </li>
+          </div>
         </div>
       </div>
-    </div>
-  </div>`;
-      $(".list-group").append(html);
-      $("#as").append(html2);
-      document.querySelectorAll(".avatarList")[i].style.backgroundImage =
-        "url('data:image/png;base64," + base64String + "')";
-    }
-  },
-});
+    </div>`;
+        $(".list-group").append(html);
+        $("#as").append(html2);
+        document.querySelectorAll(".avatarList")[i].style.backgroundImage =
+          "url('data:image/png;base64," + base64String + "')";
+      }
+    },
+  });
+  console.log("cc");
+}
+
 
 
 //配對local storage (按完接受配對or拒絕配對後當天不能再按按鈕)
@@ -316,4 +328,13 @@ const init = () => {
 
   showBtn(true);
 };
-window.addEventListener("load", init);
+
+
+// 避免要再重新刷一次頁面才會載入資訊
+window.onload = function(){
+  init();
+  pair();
+  update();
+  chat();
+}
+
