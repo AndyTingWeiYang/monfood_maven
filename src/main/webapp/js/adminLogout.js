01fdc2f7a43a19a0d59f1ee17dbea57635c28b95
wii <wii6436@gmail.com>
2022-08-15 15:35:01 +0800

    add and update admin logout series files adding admin logout function

---
+++ src/main/webapp/js/adminLogout.js
@@ -0,0 +1,23 @@
+window.addEventListener('load', function(){
+    alert("hi");
+    $(".logout").on("click", function () {
+        var logout = "/AdminLogoutServlet";
+        $.ajax({
+            url: logout,
+            type: "POST",
+            data: JSON.stringify({
+              action: "logout",
+            }),
+            dataType: "json",
+            success: function (data) {
+                console.log(data);
+                if ("登出成功" === data) {
+                    window.location.href = "adminLogin.html";
+                }
+            },
+            complete: function () {
+                window.location.href = "adminLogin.html";
+            },
+        });
+    });
+})
