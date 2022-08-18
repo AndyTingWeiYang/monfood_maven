123c263be7b4b858ef0d0a91854a25f504375aaf
yuyu <yuyuforjava@gmail.com>
2022-08-18 17:40:44 +0800

    fix mail

--- src/main/java/com/model/user/controller/AccountStatusServlet.java
+++ src/main/java/com/model/user/controller/AccountStatusServlet.java
@@ -53,4 +53,9 @@ public class AccountStatusServlet extends HttpServlet {
 		}
 
 	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doPost(request, response);
+	}
 }
